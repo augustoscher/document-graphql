42a532e7cb183235233ed4873249999a7a36358a
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-04 16:44:18 -0300

    feat: mongoose Document schema definition

---
+++ src/models/document.ts
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const documentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artifact: {
+    type: String,
+    required: true,
+    enum: ['JURISPRUDENCIA', 'PECA', 'DOUTRINA', 'ARTIGO', 'NOTICIA', 'MODELO']
+  },
+  body: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default mongoose.model('document', documentSchema, 'document')
