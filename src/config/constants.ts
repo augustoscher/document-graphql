a01822c10015cdc2dadc3415b438e83da5858674
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-05 12:30:46 -0300

    feat: using configs from env and set default confs as fallback

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,8 @@
 const {
-  NODE_ENV = 'development',
-  MONGODB_URL = 'mongodb://db:27017/graphql',
-  MONGO_USER = 'document-graphql',
-  MONGO_PASSWORD = 'document-graphql',
+  NODE_ENV = process.env.NODE_ENV || 'development',
+  MONGODB_URL = process.env.MONGO_URL || 'mongodb://db:27017/graphql',
+  MONGO_USER = process.env.MONGO_USER || 'document-graphql',
+  MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'document-graphql',
   PORT = process.env.PORT || 4002
 } = process.env
 
