9e9074f681e4e8dd73ae9f0b43b443a886db5c40
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-11-05 12:30:37 -0300

    feat: using configs from env and set default confs as fallback

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,7 @@ const {
   MONGODB_URL = 'mongodb://db:27017/graphql',
   MONGO_USER = 'document-graphql',
   MONGO_PASSWORD = 'document-graphql',
-  PORT = 4002
+  PORT = process.env.PORT || 4002
 } = process.env
 
 export { NODE_ENV, MONGODB_URL, MONGO_USER, MONGO_PASSWORD, PORT }
