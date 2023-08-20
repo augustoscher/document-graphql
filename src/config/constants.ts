073a2cf6d2c4051e0c5b5f2185ce3bcc7741983a
Augusto <augustoscher@gmail.com>
2023-08-20 18:03:51 -0300

    feat: log

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 const {
   NODE_ENV = process.env.NODE_ENV || 'development',
-  MONGODB_URL = process.env.MONGO_URL || 'mongodb://db:27017/graphql',
+  MONGODB_URL = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/graphql',
   MONGO_USER = process.env.MONGO_USER || 'document-graphql',
   MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'document-graphql',
   PORT = process.env.PORT || 4002
